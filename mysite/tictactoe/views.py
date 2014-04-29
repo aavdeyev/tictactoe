bbde69fa3a24092955eb49b41499deda48488605
aavdeyev <aavdeyev@hotmail.com>
2014-04-29 10:14:32 -0400

    Created main tictactoe view

--- mysite/tictactoe/views.py
+++ mysite/tictactoe/views.py
@@ -9,17 +9,12 @@ from django.template import RequestContext
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 
-from pwd_mgr.models import pwdMgrModel
-from pwd_mgr.forms import pwdMgrModelForm
-
 from django.utils import timezone
 from django.shortcuts import render_to_response
 from django.contrib import auth
 from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.models import User
-
-#################### App User Management functions #################
 
 def login(request) :
 
@@ -67,46 +62,9 @@ def register_user(request) :
 def register_success(request) :
 
     return render_to_response('register_success.html')
+
+def tictactoe_main(request) :
+
+    return render_to_response('tictactoe.html')
         
-##################### PWD MGR specific functions #####################
-        
-def add_user(request) :
-
-    if not request.user.is_authenticated() :
-
-        # Unathenticated user, redirect the user to the login page
-        return render_to_response('login.html',
-                context_instance=RequestContext(request))
-    
-    if request.method == 'POST' :
-
-        form = pwdMgrModelForm(request.POST)
-        
-        if form.is_valid() :        
-            model_instance = form.save(commit=False)
-
-            # Add id number for the current logged in web user
-            model_instance.owner =\
-                User.objects.get(username=request.user.username).id
-
-            # Add record creation time
-            model_instance.created = timezone.now()
-            model_instance.save()
-            
-            return render_to_response('continue.html')
-        else :            
-            return render_to_response('add_user.html',\
-                    {'form' : form.as_p(),\
-                    'username' : request.user.username},
-                    context_instance=RequestContext(request))
-
-    else:
-
-        form = pwdMgrModelForm()
-
-        return render_to_response('add_user.html',\
-                {'form' : form.as_p(),\
-                'username' : request.user.username},\
-                context_instance=RequestContext(request))
-
 
