950b25b867ff2a7a28bfc286a7e1a5197e832da0
aavdeyev <aavdeyev@hotmail.com>
2014-05-06 18:09:00 -0400

    Converted from $.getJSON to $.ajax

--- mysite/tictactoe/models.py
+++ mysite/tictactoe/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class History(models.Model):
     owner = models.IntegerField()
-    result = models.CharField(max_length=8)
+    result = models.CharField(max_length=16)
     created = models.DateTimeField()
 
     def __unicode__(self):
 
-        if self.result.lower() == 'userlost' :
+        if self.result.lower() == 'user_lost' :
             result_str = 'lost'
-        elif self.result.lower() == 'userwin' :
+        elif self.result.lower() == 'user_won' :
             result_str = 'won'
         else:
             result_str = 'draw'
