4d13956d4d64f97292c6f81a370ef51dfd5c2cd1
aavdeyev <aavdeyev@hotmail.com>
2014-05-10 14:32:32 -0400

    Added timestamp to the Ajax request so that IE doesn't cache it

--- mysite/tictactoe/urls.py
+++ mysite/tictactoe/urls.py
@@ -5,7 +5,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-
     url(r'^next_turn/$', 'tictactoe.views.play_next_turn'),
     url(r'^new_game/$', 'tictactoe.views.start_new_game'),
     url(r'^history/$', 'tictactoe.views.game_history'),
--- mysite/tictactoe/views.py
+++ mysite/tictactoe/views.py
@@ -1,6 +1,11 @@
+#
+#
+# Create your views here
+#
+#
+
 from django.shortcuts import render
 
-# Create your views here.
 from django import forms
 from django.template import RequestContext
 
